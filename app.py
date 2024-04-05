dc22d1cf2d9becd2140f848b25f5b8f342eee312
Pavel Savoskin <56044334+sneggoroddd@users.noreply.github.com>
2024-04-05 15:48:11 +0200

    Update app.py
    new path

--- app.py
+++ app.py
@@ -419,7 +419,8 @@ if __name__ == '__main__':
     parser.add_argument('--inbrowser', action='store_true', help='Open in browser')
     parser.add_argument('--server_port', type=int, default=7860, help='Server port')
     parser.add_argument('--share', action='store_true', help='Share the Gradio UI')
-    parser.add_argument('--model_path', type=str, default='stablediffusionapi/juggernaut-xl-v8', help='Base model path')
+    #parser.add_argument('--model_path', type=str, default='stablediffusionapi/juggernaut-xl-v8', help='Base model path')
+    parser.add_argument('--model_path', type=str, default='stablediffusionapi.com/api/v4/dreambooth', help='Base model path')
     parser.add_argument('--medvram', action='store_true', help='Medium VRAM settings')
     parser.add_argument('--lowvram', action='store_true', help='Low VRAM settings')
 
